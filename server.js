48044f074df9882b9de746cf825ce5159c2c910f
tracyossment <37661839+tracyossment@users.noreply.github.com>
2018-03-23 00:41:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,54 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var content={
+    title : 'Article One',
+    heading: 'Article One',
+    date:'Sept5,2018',
+    content: `<p> I hate college</p>
+    <p>booyaah one</p>`
+}
+function createTemplate(data){
+    
+    var title= data.title;
+    var date= data.date;
+    var heading= data.heading;
+    var content= data.content;
+    var htmlTemplate= ` <head> 
+                <title>
+                    ${title}
+                </title>
+                <meta name= "viewport" content="width=device-width,initial-scale=1"/>
+                <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+    <body>
+        <div class="container">
+            <div>
+                <a href= "/">Home</a>
+            </div>
+            <hr/>
+            <h3>
+               ${heading}
+            </h3>
+            <div>
+                 ${date}
+            </div>
+            <div>
+              ${content}
+            </div>
+        </div>  
+    
+       
+        
+    </body>
+    </html>`;
+    return htmltemplate;
+    
+}  
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-one',function(req,res){
