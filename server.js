937134a8a152a217df0213da2ec5fa475265967e
tracyossment <37661839+tracyossment@users.noreply.github.com>
2018-03-23 00:43:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -52,11 +52,11 @@ function createTemplate(data){
 }  
 
 app.get('/', function (req, res) {
-  res.send(createTemplate(articleOne));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
- res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+ res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function(req,res){
