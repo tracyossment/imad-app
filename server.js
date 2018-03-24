37edfdfd7c90015084c9cb08ed58a886d2030c54
tracyossment <37661839+tracyossment@users.noreply.github.com>
2018-03-24 12:35:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,8 +78,8 @@ app.get('/:articleName',function(req,res){
 });
 
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/ui/style2.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style2.css'));
 });
 
 app.get('/ui/main.js', function (req, res) {
