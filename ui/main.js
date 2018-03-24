da2aa74414155941104df7a25853119fd1d750c3
tracyossment <37661839+tracyossment@users.noreply.github.com>
2018-03-24 21:58:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,7 +18,7 @@ var button =document.getElementById('counter');
 
     console.log('not clicked');
 var counter= 0;
-button.onClick= function () {
+button.onclick= function () {
     console.log('clicked');
        counter=counter+1;
     var span=document.getElementById('count');
