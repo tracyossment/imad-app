990c787fd1a8dcadbc3f3bc534e4548ce4161fd7
tracyossment <37661839+tracyossment@users.noreply.github.com>
2018-03-24 12:32:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,10 @@
 console.log('Loaded!');
 var element= document.getElementById('main text');
 element.innerHTML='New Value';
+
+
+var img= document.getElementById('madi');
+img.onclick=function(){
+    img.style.marginLeft='100px';
+}
+
